757e1a4923dd85029eb50f5fbc9c603733eb1e12
Thomas_Bauer <thomas.bauer@qsc.com>
2022-04-20 05:29:03 +0200

    added relative path for database with print

--- webstreaming.py
+++ webstreaming.py
@@ -7,6 +7,7 @@ import sqlite3
 import os
 import sys
 print(os.chdir(sys.path[0]))
+
 #need relative path for linux
 conn = sqlite3.connect("database.db", check_same_thread=False)
 video = VideoGenerator()
